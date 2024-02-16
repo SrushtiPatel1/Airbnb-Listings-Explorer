8d60027212887b99d9281134009dbcbc7474b744
Srushti Patel <srushti.16patel@gmail.com>
2024-02-16 14:27:01 -0500

    integrity update

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,14 @@
+/*********************************************************************************
+*  WEB422 – Assignment 3
+*
+*  I declare that this assignment is my own work in accordance with Seneca's
+*  Academic Integrity Policy:
+* 
+*  https://www.senecapolytechnic.ca/about/policies/academic-integrity-policy.html
+* 
+*  Name: Srushti Patel Student ID: 117791228 Date: 16/02/2024
+*
+********************************************************************************/ 
 import { useState, useEffect } from 'react';
 import useSWR from 'swr';
 import { Pagination, Accordion } from 'react-bootstrap';
